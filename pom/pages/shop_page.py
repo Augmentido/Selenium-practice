371f36d54b3c3ee13c8ac93546bce51f0d352237
Denys Konovalov <sandulfda@gmail.com>
2023-05-08 01:34:06 -0700

    Testcase for testting searchbar

--- pom/pages/shop_page.py
+++ pom/pages/shop_page.py
@@ -18,7 +18,16 @@ class ShopPage:
     _product_increment_button = (By.CSS_SELECTOR, ".stepper-input .increment")
     _product_decrement_button = (By.CSS_SELECTOR, ".stepper-input .decrement")
     _product_count_tocart_input = (By.CSS_SELECTOR, ".stepper-input input.quantity")
+    _product_name = (By.CSS_SELECTOR, ".product-name")
+
+    # Some element at the end of the page
     _page_loaded_element = (By.CSS_SELECTOR, "footer")
+
+    # Selectors for search
+    _search_form = (By.CSS_SELECTOR, "header form.search-form")
+    _search_input = (By.CSS_SELECTOR, "input[type=\"search\"]")
+    _search_button = (By.CSS_SELECTOR, "button[type=\"submit\"]")
+
 
     def __init__(self, browser):
         self._browser = browser
@@ -29,12 +38,14 @@ class ShopPage:
     def get_product_dom_element(self, index=0):
         """
         Finds and returns a DOM element containing the product (from products list)
-        :param index: element number
+        :param index: element number OR -1 to get list of all elements
         :return: DOM Element
         """
-        if index < 0:
+        if index < -1:
             return None
         els = self._browser.find_elements(self._product_locator[0], self._product_locator[1])
+        if index == -1:
+            return els
         return els[index] if index < len(els) else None
 
     def get_product_tocart_quantity(self, product_object):
@@ -64,3 +75,41 @@ class ShopPage:
         dec_button = product_object.find_element(self._product_decrement_button[0], self._product_decrement_button[1])
         dec_button.click()
 
+    def get_search_form_dom_element(self):
+        """
+        Finds and returns a DOM element of search form
+        :return: DOM Element
+        """
+        return self._browser.find_element(self._search_form[0], self._search_form[1])
+
+    def send_keyword_to_search_bar(self, search_form, keyword):
+        """
+        Inputs some text to search bar
+        :param search_form: search FORM DOM element
+        :param keyword: text to send
+        :return: None
+        """
+        input_el = search_form.find_element(self._search_input[0], self._search_input[1])
+        input_el.clear()
+        input_el.send_keys(keyword)
+
+    def click_search_go_button(self, search_form):
+        """
+        Clicks on "GO" button on search bar
+        :param search_form: search FORM DOM element
+        :return: None
+        """
+        go_button = search_form.find_element(self._search_button[0], self._search_button[1])
+        go_button.click()
+
+    def get_product_names(self):
+        """
+        Retrives the list of product names on page.
+        :return: list of product names on page
+        """
+        product_doms = self.get_product_dom_element(-1)
+        names_list = []
+        for product_dom in product_doms:
+            el = product_dom.find_element(self._product_name[0], self._product_name[1])
+            names_list.append(el.text)
+        return names_list
--- tests/test_shopPage.py
+++ tests/test_shopPage.py
@@ -1,6 +1,7 @@
 """ tests for home page """
 from pytest import mark
 from pom.pages.shop_page import ShopPage
+import time
 
 
 @mark.functional
@@ -24,7 +25,7 @@ class TestShopPage:
 
     def test_product_count_decrement_btn(self, browser):
         """
-        Check if the "minus" button decrements the count inside the product block
+        Check if the "minus" button decrements count inside the product block
         :param browser: webdriver
         :return: None
         """
@@ -35,4 +36,56 @@ class TestShopPage:
         cnt = page.get_product_tocart_quantity(product_obj)
         page.click_product_decrement_button(product_obj)
         assert page.get_product_tocart_quantity(product_obj) == cnt - 1
-        
+
+    def test_product_search(self, browser):
+        """
+        Check if the searchbar works properly
+        :param browser: webdriver
+        :return: None
+        """
+        page = ShopPage(browser)
+        search_form = page.get_search_form_dom_element()
+
+        # check search by writing some keyword and pressing "Enter"
+        page.send_keyword_to_search_bar(search_form, "be\x0D")
+        time.sleep(3)
+        names = page.get_product_names()
+        for name in names:
+            assert "be" in name.lower()
+
+        # check search by writing some keyword and clicking on "GO" button
+        page.send_keyword_to_search_bar(search_form, "ca")
+        page.click_search_go_button(search_form)
+        time.sleep(3)
+        names = page.get_product_names()
+        for name in names:
+            assert "ca" in name.lower()
+
+    def test_add_to_cart(self, browser):
+        """
+        Check if add to cart in product block works properly
+        :param browser: webdriver
+        :return: None
+        """
+        # add to cart
+        #TODO
+        pass
+
+    def test_remove_from_cart(self, browser):
+        """
+        Check if remove from cart works properly
+        :param browser: webdriver
+        :return: None
+        """
+        # remove from cart
+        #TODO
+        pass
+
+
+
+# to checkout
+
+# check totals in checkout
+# place order
+# select country and checkbox
+# get "order placed" message
